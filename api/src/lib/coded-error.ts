dc80fd6bc5ecaf72334b122f712dd533c5bf15d0
sandbochs <sandbochs@github>
2021-07-13 20:13:29 -0700

    validate inventory params

---
+++ api/src/lib/coded-error.ts
@@ -0,0 +1,35 @@
+type CodedErrorParams = {
+  code: number, // error code
+  status: number, // http status code
+  message: string, // error message
+}
+
+export class CodedError extends Error {
+  code: number;
+  message: string;
+  status: number;
+
+  constructor(params: CodedErrorParams) {
+    super();
+
+    this.code = params.code;
+    this.message = params.message;
+    this.status = params.status;
+  }
+}
+
+export const errors = {
+  // Generic API errors
+  api: {
+  INVALID_TIME: { code: 600, status: 400, message: `'time' must be a ISO-8601 date string formatted as HH:MM` },
+  },
+
+  // Inventory Specific Errors
+  inventory: {
+    INVALID_INTERVAL: { code: 1000, status: 400, message: 'Invalid time, reservations can only be accepted in 15 minute intervals' },
+    ALREADY_EXISTS: { code: 1001, status: 400, message: 'Inventory already exists for the specified time and party size' },
+    MISSING_PARAMS: { code: 1002, status: 400, message: `'time', 'maxSize' and 'maxParties' must be provided` },
+    INVALID_MAX_SIZE: { code: 1003, status: 400, message: `'maxSize' must be a number` },
+    INVALID_MAX_PARTIES: { code: 1004, status: 400, message: `'maxParties' must be a number` },
+  }
+}
--- api/src/models/Inventory.ts
+++ api/src/models/Inventory.ts
@@ -2,22 +2,40 @@ import {
   AllowNull,
   Column,
   CreatedAt,
+  DataType,
+  Index,
   Model,
   PrimaryKey, Table,
 } from 'sequelize-typescript'
 
-@Table({ tableName: 'inventories' })
+import { CodedError, errors as allErrors } from '../lib/coded-error'
+import {
+  isTimeString,
+  isValidInterval,
+} from '../lib/validations';
+
+// TODO: should be configurable
+const reservationIntervalMinutes = 15;
+
+const { inventory: errors, api: apiErrors } = allErrors;
+
+@Table({
+  tableName: 'inventories',
+  indexes: [
+    {
+      unique: true,
+      fields: ['time', 'maxSize'],
+    }
+  ]
+})
 export class Inventory extends Model<Inventory> {
   @PrimaryKey
   @Column({ autoIncrement: true })
   id: number
 
-  // Defined in ./relations.ts
-  // @HasMany(() => Reservations)
-
   @AllowNull(false)
-  @Column
-  time: Date
+  @Column(DataType.TIME)
+  time: string
 
   @AllowNull(false)
   @Column
@@ -29,4 +47,27 @@ export class Inventory extends Model<Inventory> {
 
   @CreatedAt
   createdAt: string
+
+  static validate(params: { [key: string]: any }): void {
+    const { time, maxSize, maxParties } = params;
+    if (time == null || maxSize == null || maxParties == null) {
+      throw new CodedError(errors.MISSING_PARAMS)
+    }
+
+    if (!isTimeString(time)) {
+      throw new CodedError(apiErrors.INVALID_TIME)
+    }
+
+    if (!isValidInterval(time, reservationIntervalMinutes)) {
+      throw new CodedError(errors.INVALID_INTERVAL)
+    }
+
+    if(typeof maxSize !== 'number') {
+      throw new CodedError(errors.INVALID_MAX_SIZE)
+    }
+
+    if(typeof maxParties !== 'number') {
+      throw new CodedError(errors.INVALID_MAX_PARTIES)
+    }
+  }
 }
